39ba7016732eda99579cdfb86b59c40df7300b08
Anders Bolin <anders@bolin.nu>
2017-05-13 20:58:16 +0200

    First commit

---
+++ AzadHarVerkstad/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzadHarVerkstad.Startup))]
+namespace AzadHarVerkstad
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
